7584c81121dbc8f27ca99396082d2e63b6e68842
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-11-19 18:58:30 +0530

    Add files via upload

---
+++ gradeofstudent.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner S=new Scanner(System.in);
+		int marks1,marks2,marks3;
+		marks1=S.nextInt();
+		if(marks1>90)
+		{
+		    System.out.println("A");
+		}
+		else if(marks1>70)
+		{
+		    System.out.println("B");
+		}
+		else if(marks1>=40)
+		{
+		    System.out.println("C");
+		    
+		}
+		else{
+		    System.out.println("F");
+		}
+		marks2=S.nextInt();
+		if(marks2>90)
+		{
+		    System.out.println("A");
+		}
+		else if(marks2>70)
+		{
+		    System.out.println("B");
+		}
+		else if(marks2>=40)
+		{
+		    System.out.println("C");
+		    
+		}
+		else{
+		    System.out.println("F");
+		}
+		marks3=S.nextInt();
+		if(marks3>90)
+		{
+		    System.out.println("A");
+		}
+		else if(marks3>70)
+		{
+		    System.out.println("B");
+		}
+		else if(marks3>=40)
+		{
+		    System.out.println("C");
+		    
+		}
+		else{
+		    System.out.println("F");
+		}
+
+	}
+}
